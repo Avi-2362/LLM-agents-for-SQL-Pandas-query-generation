254972586625e91b1cf2d38130894ecb1021f276
md-hassan <mdhassan@umass.edu>
2024-12-05 13:25:26 -0500

    fixes unit tests due to slight change in API

--- 520_Project/backend/tests/api_models.py
+++ 520_Project/backend/tests/api_models.py
@@ -6,15 +6,15 @@ from models import *
 
 # test the User model
 def test_user_model():
-    user = User(user_id='123', name='John Doe', username='johndoe', email='john.doe@example.com')
+    user = User(user_id='123', name='John Doe', username='johndoe', email='john.doe@example.com', hashed_password='password')
     user.put()  # Insert into USER_TABLE
-    assert User.get('123') == {'user_id': '123', 'name': 'John Doe', 'username': 'johndoe', 'email': 'john.doe@example.com'}
-    assert user.to_json() == '{"user_id": "123", "name": "John Doe", "username": "johndoe", "email": "john.doe@example.com"}'
+    assert User.get('123') == {'user_id': '123', 'name': 'John Doe', 'username': 'johndoe', 'hashed_password': 'password', 'email': 'john.doe@example.com'}
+    assert user.to_json() == '{"user_id": "123", "name": "John Doe", "username": "johndoe", "hashed_password": "password", "email": "john.doe@example.com"}'
 
 # test the UserFiles model
 def test_user_files_model():
-    user_files = UserFiles(user_id='123', file_ids=['file1', 'file2'])
+    user_files = UserFiles(user_id='123', files=['file1', 'file2'])
     user_files.put()  # Insert into USER_FILES_TABLE
     print(UserFiles.get('123'))
-    assert UserFiles.get('123') == {'user_id': '123', 'file_ids': ['file1', 'file2']}
-    assert user_files.to_json() == '{"user_id": "123", "file_ids": ["file1", "file2"]}'
+    assert UserFiles.get('123') == {'user_id': '123', 'files': ['file1', 'file2']}
+    assert user_files.to_json() == '{"user_id": "123", "files": ["file1", "file2"]}'
